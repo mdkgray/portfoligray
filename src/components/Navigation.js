4f7820d2e3c2d3d669a6652ba63734dcafe05393
Mackenzie Gray <mdkulen@gmail.com>
2022-09-30 09:42:15 +1000

    added images to folder

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 export default function Navigation({ currentPage, handlePageChange }) {
 return (
         <ul className="">
