f8533a70be79b793420b552935a54c1c8ed2ba40
Mackenzie Gray <mdkulen@gmail.com>
2022-09-30 16:52:26 +1000

    updated PageContainer.js

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,34 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import "../App.css";
 
 import About from '../components/Pages/About';
 import Contact from '../components/Pages/Contact';
 import Portfolio from '../components/Pages/Portfolio';
 import Resume from '../components/Pages/Resume';
-import Navigation from './Navigation';
+import PageContent from './PageContent';
 
-export default function ProfileContainer(currentPage, setCurrentPage) {
-    // const [currentPage, setCurrentPage] = useState('About');
+export default function ProfileContainer({currentPage}) {    
 
     const renderPage = () => {
-        if (currentPage === 'About') {
+        if (currentPage.name === 'About') {
             return <About />;
         }
-        if (currentPage === 'Portfolio') {
+        if (currentPage.name === 'Portfolio') {
             return <Portfolio />;
         }
-        if (currentPage === 'Resume') {
+        if (currentPage.name === 'Resume') {
             return <Resume />;
         }
         return <Contact />;
     };
 
-    const handlePageChange = (page) => setCurrentPage(page);
-
     return (
         <div>
-            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-            {renderPage()}
+            <h2>{currentPage.name}</h2>
+            <PageContent>{renderPage()}</PageContent>
         </div>
     );
 }
