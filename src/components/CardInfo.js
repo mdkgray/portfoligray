8d6ec7c2c2d08ea37e437b94097fb223942166ac
Mackenzie Gray <mdkulen@gmail.com>
2022-10-02 16:14:00 +1100

    modified projects display with cardInfo

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -6,50 +6,29 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
 export default function CardInfo(props) {
     return (
-        <div>       
-            <Box sx={{ width: '100%' }}>
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={4}>
-                        <Item>
-                            <Card sx={{ maxWidth: 400 }}>
-                                <CardMedia
-                                    component="img"
-                                    height="140"
-                                    image={props.image}
-                                    alt={''}
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {props.name}
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        {props.description}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <a href={props.sourceCode}><Button size="small" variant="contained">GitHub</Button></a>
-                                    <a href={props.livePreview}><Button size="small" variant="outlined">Deployed</Button></a>
-                                </CardActions>
-                            </Card>
-                        </Item>
-                    </Grid>
-                </Grid>
-            </Box>
+        <div>
+            <Card sx={{ maxWidth: 400 }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={props.image}
+                    alt={''}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <a href={props.sourceCode}><Button size="small" variant="contained">GitHub</Button></a>
+                    <a href={props.livePreview}><Button size="small" variant="outlined">Deployed</Button></a>
+                </CardActions>
+            </Card> 
         </div>
     )
 }
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -2,25 +2,46 @@ import React from 'react';
 import { projects } from '../../content';
 import CardInfo from '../CardInfo';
 
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
 export default function Projects() {
     console.log(projects);
 
     return (
-    <div>
-        <h1>Projects</h1>
         <div>
-            {projects.map((project, i) => (
-                <CardInfo
-                    key={i}
-                    name={project.name}
-                    image={project.image}
-                    description={project.description}
-                    technologies={project.technologies}
-                    sourceCode={project.sourceCode}
-                    livePreview={project.livePreview}
-                />
-            ))}
+            <h1>Projects</h1>
+            <Box sx={{ width: '100%' }}>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
+                        <Item>
+                        <div>
+                            {projects.map((project, i) => (
+                                <CardInfo
+                                    key={i}
+                                    name={project.name}
+                                    image={project.image}
+                                    description={project.description}
+                                    technologies={project.technologies}
+                                    sourceCode={project.sourceCode}
+                                    livePreview={project.livePreview}
+                                />
+                            ))}
+                        </div>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
-    </div>
     );
 }
