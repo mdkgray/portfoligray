3c685afb2b2d1c21552401fadc63e123631b9095
Mackenzie Gray <mdkulen@gmail.com>
2022-10-02 15:47:19 +1100

    modified projects display

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export default function CardInfo(props) {
     console.log(props);
 
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>{props.description}</p>
-            <p>{props.technologies}</p>
-            <a href={props.sourceCode}>GitHub</a>
-            <br/>
-            <a href={props.livePreview}>Live Preview</a>
-            <img src={props.image} alt={''}></img>
+            <Card sx={{ maxWidth: 400 }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={props.image}
+                    alt={''}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <a href={props.sourceCode}><Button size="small" variant="contained">GitHub</Button></a>
+                    <a href={props.livePreview}><Button size="small" variant="outlined">Deployed</Button></a>
+                </CardActions>
+            </Card> 
         </div>
     )
 }
