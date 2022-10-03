5d2189182576a9041c8f1c65b17c1929a599f680
Mackenzie Gray <mdkulen@gmail.com>
2022-10-03 12:45:31 +1100

    added skills and resume section to Resume.js

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { skills } from '../../content';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 export default function Resume() {
     return (
-    <div>
-        <h1>Resume</h1>
-    </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} padding={4}>
+                <Grid item xs={4}>
+                    <Item>
+                        <h5>Check out my resume:</h5>
+                    </Item>
+                </Grid>
+                <Grid item xs={8}>
+                    <h5>Skills</h5>
+                    <Item>
+                        <ImageList sx={{ width: 500, height: 450 }} cols={4} rowHeight={164}>
+                        {skills.map((skills) => (
+                            <ImageListItem key={skills.image}>
+                            <img
+                                src={`${skills.image}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${skills.image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                alt={skills.name}
+                                loading="lazy"
+                            />
+                            </ImageListItem>
+                        ))}
+                        </ImageList>
+                    </Item>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
 
