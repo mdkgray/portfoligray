93411801ae76a8b2a5a0bd5469598b53c8e11131
Mackenzie Gray <mdkulen@gmail.com>
2022-10-03 12:11:57 +1100

    layout projects page

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -9,27 +8,29 @@ import Typography from '@mui/material/Typography';
 
 export default function CardInfo(props) {
     return (
-        <div>
-            <Card>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image={process.env.PUBLIC_URL + props.image}
-                    alt={'projectImage'}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {props.name}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.description}
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <a href={props.sourceCode} target="_blank" rel="noopener noreferrer"><Button size="small" variant="contained">GitHub</Button></a>
-                    <a href={props.livePreview} target="_blank" rel="noopener noreferrer"><Button size="small" variant="outlined">Deployed</Button></a>
-                </CardActions>
-            </Card> 
-        </div>
+        <Card>
+            <CardMedia
+                component="img"
+                height="140"
+                image={process.env.PUBLIC_URL + props.image}
+                alt={'projectImage'}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {props.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.description}
+                </Typography>
+                <br />
+                <Typography variant="body2" color="text.secondary">
+                    {props.technologies}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <a href={props.sourceCode} target="_blank" rel="noopener noreferrer"><Button size="small" variant="contained">GitHub</Button></a>
+                <a href={props.livePreview} target="_blank" rel="noopener noreferrer"><Button size="small" variant="outlined">Deployed</Button></a>
+            </CardActions>
+        </Card> 
     )
 }
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { projects } from '../../content';
 import CardInfo from '../CardInfo';
-import { styled } from '@mui/material/styles';
+import { experimentalStyled as styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -10,40 +9,34 @@ import Box from '@mui/material/Box';
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
-    padding: theme.spacing(1),
+    padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
 }));
 
 export default function Projects() {
     return (
-        <div className="container-fluid text-center">
-            <div className="row d-flex">
-                <div className="col-md-4">
-                    <h1>Projects</h1>
-                    <Box >
-                        <Grid >
-                            <Grid >
-                                <Item>
-                                    <div>
-                                        {projects.map((project, i) => (
-                                            <CardInfo
-                                                key={i}
-                                                name={project.name}
-                                                image={project.image}
-                                                description={project.description}
-                                                technologies={project.technologies}
-                                                sourceCode={project.sourceCode}
-                                                livePreview={project.livePreview}
-                                            />
-                                        ))}
-                                    </div>
-                                </Item>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </div>                
-            </div>            
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                columns={{ xs: 1, sm: 8, md: 12 }}>
+                {projects.map((project, i) => (
+                    <Grid item xs={2} sm={4} md={4} key={i}>
+                        <Item>
+                            <CardInfo
+                                key={i}
+                                name={project.name}
+                                image={project.image}
+                                description={project.description}
+                                technologies={project.technologies}
+                                sourceCode={project.sourceCode}
+                                livePreview={project.livePreview}
+                            />
+                        </Item>
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
     );
 }
