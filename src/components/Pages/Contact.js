8f93cc83300426dd24ba1cf246daa5fd533c16a1
Mackenzie Gray <mdkulen@gmail.com>
2022-10-03 18:33:01 +1100

    styling page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+import { about } from '../../content';
+import { styled } from '@mui/material/styles';
+
 import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -11,63 +14,97 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import EmailIcon from '@mui/icons-material/Email';
-import { about } from '../../content';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#001C55' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));  
 
 
 export default function Contact() {
     return (
-        <React.Fragment className="container">
-            <CssBaseline />
-            <Container className="container d-flex flex-direction-column col-md-11 justify-content-center mt-5">
-                <Box className="container row col-md-5">
-                    <h1>Contact</h1>
-                    <p>Get in touch</p>
-                    <div>
-                        <Link href="mailto:mdkulen@gmail.com" underline="none">
-                            <Stack spacing={2} direction="row" centered={'true'}>
-                                <Button variant="contained" className="mx-auto"><EmailIcon className="m-1"/>mdkulen@gmail</Button>
-                            </Stack>
-                        </Link>
-                    </div>
-                    <div>
-                        <a href={about.social.github} target="_blank" rel="noopener noreferrer" className="mx-2"><GitHubIcon /></a>
-                        <a href={about.social.linkedin} target="_blank" rel="noopener noreferrer" className="mx-2"><LinkedInIcon /></a>
-                        <a href={about.social.twitter} target="_blank" rel="noopener noreferrer" className="mx-2"><TwitterIcon /></a>
-                    </div>
-                </Box>
-                <Box
-                    component="form"
-                    sx={{ '& .MuiTextField-root': { m: 1, width: '50ch'}}}
-                    noValidate
-                    autoComplete="off"
-                    className="container row col-md-6 justify-content-center"
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid 
+                container 
+                spacing={3}
+                padding={4}
+                display="flex" 
+                justifyContent="center" 
                 >
-                    <div className='col-md-9'>
-                        <TextField
-                            id="outlined-textarea"
-                            label="Name"
-                            placeholder="Enter Name"
-                            multiline
-                        />
-                        <TextField
-                            id="outlined-textarea"
-                            label="Email"
-                            placeholder="example@example.com"
-                            multiline
-                        />
-                        <TextField
-                            id="outlined-multiline-static"
-                            label="Message"
-                            multiline
-                            rows={5}
-                        />
+                <Grid item xs={11} md={4}>
+                    <Item>
+                        <Box 
+                            spacing={3}
+                            padding={4}
+                            margin={5}
+                            >
+                            <h1>Lets Connect</h1>
+                            <Box 
+                                padding={4}
+                                margin={5}
+                                >
+                                <Link href="mailto:mdkulen@gmail.com" underline="none">
+                                    <Stack spacing={2} direction="row" >
+                                        <Button variant="contained"><EmailIcon className="m-1"/>mdkulen@gmail</Button>
+                                    </Stack>
+                                </Link>
+                            </Box>
+                            <Box 
+                                margin={2}>
+                                <a href={about.social.github} target="_blank" rel="noopener noreferrer" className="mx-2"><GitHubIcon /></a>
+                                <a href={about.social.linkedin} target="_blank" rel="noopener noreferrer" className="mx-2"><LinkedInIcon /></a>
+                                <a href={about.social.twitter} target="_blank" rel="noopener noreferrer" className="mx-2"><TwitterIcon /></a>
+                            </Box>
+                        </Box>
+                    </Item>
+                </Grid>
 
-                        <Stack spacing={2} direction="row">
-                            <Button variant="contained" className="mx-auto">Submit</Button>
-                        </Stack>
-                    </div>
-                </Box>                
-            </Container>
-        </React.Fragment>        
+                <Grid 
+                    item xs={11} 
+                    md={7}
+                    container
+                    >
+                    <Item>
+                        <Box
+                            component="form"
+                            sx={{ '& .MuiTextField-root': { m: 1, width: '50ch'}}}
+                            noValidate
+                            autoComplete="off"
+                            display="flex" 
+                            justifyContent="center"
+                            >
+                            <Box 
+                                padding={2}
+                                margin={1}>
+                                <TextField
+                                    id="outlined-textarea"
+                                    label="Name"
+                                    placeholder="Enter Name"
+                                    multiline
+                                />
+                                <TextField
+                                    id="outlined-textarea"
+                                    label="Email"
+                                    placeholder="example@example.com"
+                                    multiline
+                                />
+                                <TextField
+                                    id="outlined-multiline-static"
+                                    label="Message"
+                                    multiline
+                                    rows={5}
+                                />
+                                <Stack spacing={2} direction="row" margin={3}>
+                                    <Button variant="contained" className="mx-auto">Submit</Button>
+                                </Stack>
+                            </Box>
+                        </Box>
+                    </Item>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { skills } from '../../content';
+import { styled } from '@mui/material/styles';
+
+import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#001C55' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+})); 
 
 export default function Resume() {
     return (
@@ -24,31 +35,34 @@ export default function Resume() {
                     <h2>Resume</h2>
                 </Grid>
 
-                <Box columns={{ xs: 2, sm: 4, md: 6 }}>
-                    <h2>Skills</h2>
-                    <Grid 
-                        container 
-                        spacing={{ xs: 2, md: 3 }} 
-                        columns={{ xs: 2, sm: 4, md: 6 }}
-                        padding={4}
-                        margin={3}
-                        display="flex" 
-                        justifyContent="center" 
-                        alignItems="center"
-                        >
-                        {skills.map((skills) => (
-                            <img
-                                src={process.env.PUBLIC_URL + `${skills.image}`}
-                                srcSet={process.env.PUBLIC_URL + `${skills.image}`}
-                                alt={skills.name}
-                                height="150"
-                                width="150"
-                                spacing={{ xs: 2, md: 3 }}
-                                padding={{ xs: 2, md: 3 }}
-                            />
-                        ))}
-                    </Grid>
-                </Box>
+                <Item>
+                    <Box columns={{ xs: 2, sm: 4, md: 6 }}>
+                        <h2>Skills</h2>
+                        <Grid 
+                            container 
+                            spacing={{ xs: 2, md: 3 }} 
+                            columns={{ xs: 2, sm: 4, md: 6 }}
+                            padding={4}
+                            margin={3}
+                            display="flex" 
+                            justifyContent="center" 
+                            alignItems="center"
+                            >
+                            {skills.map((skills) => (
+                                <img
+                                    src={process.env.PUBLIC_URL + `${skills.image}`}
+                                    srcSet={process.env.PUBLIC_URL + `${skills.image}`}
+                                    alt={skills.name}
+                                    height="150"
+                                    width="150"
+                                    spacing={{ xs: 2, md: 3 }}
+                                    padding={{ xs: 2, md: 3 }}
+                                />
+                            ))}
+                        </Grid>
+                    </Box>
+                </Item>
+                
             </Grid>
         </Container>
     );
