d59aab4f19e534b4b956cdb6fdc97e2a32ba7d75
Mackenzie Gray <mdkulen@gmail.com>
2022-10-03 11:28:45 +1100

    changed layout of technologies

--- src/content.js
+++ src/content.js
@@ -20,7 +20,7 @@ const projects = [
     {
         name: 'Check-A-Tradie', 
         description: 'Check-A-Tradie allows a tradesperson looking for work to display their services, skills and certification so that a consumer can browse a list of options for a service they need completing. A business owner can sign up and register their business, edit their dashboard and profile to showcase what they offer. Our application also allows consumers to be able to browse the selection of businesses and select one based on the job they require to be completed.', 
-        technologies: ['Node.js', 'Express.js', 'MySQL', 'Sequelize', 'Javascript', 'Handlebars.js', 'Bootstrap', 'npm', 'Heroku'], 
+        technologies: ['Node.js | Express.js | MySQL | Sequelize | Javascript | Handlebars.js | Bootstrap | npm | Heroku'], 
         sourceCode: 'https://github.com/mdkgray/check-a-tradie', 
         livePreview: 'https://pacific-waters-66801.herokuapp.com/',
         image: '/assets/projectImages/check_a_tradie.png'
@@ -28,7 +28,7 @@ const projects = [
     {
         name: 'Employee Tracker', 
         description: 'A command line application that can be used by a business owner to view and manage the departments, roles and employees in the company. Data in the database can be added, deleted and modified using prompts presented to the user in the command line.', 
-        technologies: ['Javascript', 'MySQL', 'npm'], 
+        technologies: ['Javascript | MySQL | npm'], 
         sourceCode: 'https://github.com/mdkgray/employee_tracker', 
         livePreview: 'https://github.com/mdkgray/employee_tracker',
         image: '/assets/projectImages/employee_tracker.png'
@@ -36,7 +36,7 @@ const projects = [
     {
         name: 'Tech Blog', 
         description: 'A blog site for a developer that writes about technology. They can publish articles, posts, thoughts and opinions which can be viewed and commented on by other developers and application users.', 
-        technologies: ['Node.js', 'Express.js', 'MySQL', 'Sequelize', 'Javascript', 'Handlebars.js', 'Bootstrap', 'npm', 'Heroku'], 
+        technologies: ['Node.js | Express.js | MySQL | Sequelize | Javascript | Handlebars.js | Bootstrap | npm | Heroku'], 
         sourceCode: 'https://github.com/mdkgray/tech_blog', 
         livePreview: 'https://frozen-basin-60308.herokuapp.com/',
         image: '/assets/projectImages/tech_blog.png'
@@ -44,7 +44,7 @@ const projects = [
     {
         name: 'Weather Dashboard', 
         description: 'An application to be used by a traveler to check the weather outlook for multiple cities. This will allow the user to plan their trip according to the weather conditions of a certain destination.', 
-        technologies: ['OpenWeather One Call API', 'Moment.js', 'JQuery', 'Javascript', 'CSS', 'Bootstrap', 'HTML'], 
+        technologies: ['HTML | Moment.js | JQuery | Javascript | CSS | Bootstrap | OpenWeather One Call API', ], 
         sourceCode: 'https://github.com/mdkgray/weather_dashboard', 
         livePreview: 'https://mdkgray.github.io/weather_dashboard/',
         image: '/assets/projectImages/weather_app.png'
@@ -52,7 +52,7 @@ const projects = [
     {
         name: 'Note Taker', 
         description: 'A note taker application to allow business owners to write and save notes in order to keep track of tasks that need to be completed.', 
-        technologies: ['Express.js', 'Node.js', 'Javascript', 'CSS', 'HTML'], 
+        technologies: ['Express.js | Node.js | Javascript | CSS | HTML'], 
         sourceCode: 'https://github.com/mdkgray/note_taker', 
         livePreview: 'https://calm-sea-51607.herokuapp.com/,',
         image: '/assets/projectImages/note_taker.png'
@@ -60,7 +60,7 @@ const projects = [
     {
         name: 'Team Profile Generator', 
         description: 'displays the  basic information of a work team in a HTML file. Using question prompts in the terminal, information such as email address and GitHub profiles can be generated and clicked to send an email or view the GitHib profile.', 
-        technologies: ['Express.js', 'Node.js', 'npm', 'Javascript', 'CSS', 'HTML'], 
+        technologies: ['Express.js | Node.js | Javascript | CSS | HTML | npm'], 
         sourceCode: 'https://github.com/mdkgray/team_profile_generator', 
         livePreview: 'https://github.com/mdkgray/team_profile_generator',
         image: '/assets/projectImages/team_generator.png'
