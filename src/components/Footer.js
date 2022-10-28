9647bbccad6a9d183ce26a20651d73928b96e61b
Mackenzie Gray <mdkulen@gmail.com>
2022-10-28 11:35:58 +1100

    modified footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,39 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { about } from '../content';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import { about } from '../content';
+
+const Item = styled(Paper)(({ theme }) => ({
+    // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function Footer() {
     return (
-    <div className="m-3">
-        <a href={about.social.github} target="_blank" rel="noopener noreferrer" className="mx-4"><GitHubIcon /></a>
-        <a href={about.social.linkedin} target="_blank" rel="noopener noreferrer" className="mx-4"><LinkedInIcon /></a>
-        <a href={about.social.twitter} target="_blank" rel="noopener noreferrer" className="mx-4"><TwitterIcon /></a>
-    </div>
+    <Box sx={{ width: '100%', maxWidth: 'auto', display: 'flex', justifyContent: 'center', position: 'fixed', bottom: 20, height: 'auto' }}>
+        <Stack 
+            direction={{ xs: 'row', sm: 'row' }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+            >
+            <Item>
+                <a href={about.social.github} target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
+            </Item>
+            <Item>
+                <a href={about.social.linkedin} target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
+            </Item>
+            <Item>
+                <a href={about.social.twitter} target="_blank" rel="noopener noreferrer"><TwitterIcon /></a>
+            </Item>
+        </Stack>        
+    </Box>
     )
 }
 
